3d43950b2f2204117782334cac743ffc70abcf8e
Guilherme Martins <guilhermealmartins@gmail.com>
2022-07-28 14:30:48 -0300

    Final de Seção 3: EF Core

--- Back/src/MyEventos.API/Controllers/EventoController.cs
+++ Back/src/MyEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyEventos.API.Data;
 using MyEventos.API.Models;
 
 namespace MyEventos.API.Controllers
@@ -13,43 +14,25 @@ namespace MyEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
-        public EventoController()
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
-
+            _context = context;
+            
         }
-
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento() {
-                EventoId = 1,
-                Tema = "Angular",
-                Local = "Goiânia",
-                Lote = "Lote 1",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "foto.png"
-            },
-            new Evento() {
-                EventoId = 2,
-                Tema = "CSharp",
-                Local = "Goiânia",
-                Lote = "Lote 2",
-                QtdPessoas = 300,
-                DataEvento = DateTime.Now.AddDays(4).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-            },
-        };
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
 
         }
         
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]

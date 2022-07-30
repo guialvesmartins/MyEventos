c756ec0e9fa621bf31be818fde13e8c5193fc8b9
Guilherme Martins <guilhermealmartins@gmail.com>
2022-07-30 17:50:00 -0300

    Final de Seção 5: Angular

--- Front/MyEventos-App/src/app/app.module.ts
+++ Front/MyEventos-App/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/MyEventos-App/src/app/eventos/eventos.component.ts
+++ Front/MyEventos-App/src/app/eventos/eventos.component.ts
@@ -4,23 +4,51 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public isMenuCollapsed = true;
+  public eventosFiltrados: any = [];
+  largImg: number = 150;
+  margImg: number = 2;
+  mostrarImg: boolean = true;
+  private _filtroLista: string = '';
 
-  constructor(private http:HttpClient) { }
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any)  => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getEventos();
   }
 
-  public getEventos(): void{
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-    );
+  exibirOcultar(): void{
+    this.mostrarImg = !this.mostrarImg
   }
 
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+      (error) => console.log(error)
+    );
+  }
 }

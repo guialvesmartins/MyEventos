e73663db088354e7b9d4baf5c9a925a60e657208
Guilherme Martins <guilhermealmartins@gmail.com>
2022-08-03 11:38:04 -0300

    Final de Seção 6 .NetCore - Camadas

---
+++ Back/src/MyEventos.Persistence/Contextos/MyEventosContext.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyEventos.Domain;
+
+namespace MyEventos.Persistence.Contextos
+{
+    public class MyEventosContext : DbContext
+    {
+        public MyEventosContext(DbContextOptions<MyEventosContext> options) : base(options){}
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<Palestrante> Palestrantes { get; set; }
+        public DbSet<PalestranteEvento> PalestrantesEventos { get; set; }
+        public DbSet<RedeSocial> RedesSociais { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<PalestranteEvento>()
+                .HasKey(PE => new { PE.EventoId, PE.PalestranteId });
+
+            modelBuilder.Entity<Evento>()
+                .HasMany(e => e.RedesSociais)
+                .WithOne(rs => rs.Evento)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Palestrante>()
+                .HasMany(p => p.RedesSociais)
+                .WithOne(rs => rs.Palestrante)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
